1f5ccc6b6b1e28da0e45d739d42046ae302f81f3
Jerry Lucas <jerrylcas@gmail.com>
2024-10-04 13:27:14 +0700

    feat: add suspense and fetching data

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,11 +1,12 @@
-import MealsItem from "./meal-item";
+import MealItem from './meal-item';
+import classes from './meals-grid.module.css';
 
 export default function MealsGrid({ meals }) {
   return (
-    <ul>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
-          <MealsItem {...meal} />
+          <MealItem {...meal} />
         </li>
       ))}
     </ul>

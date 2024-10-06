8f21debc84380a4c18f216df7fa4eb5c22e32dc6
Jerry Lucas <jerrylcas@gmail.com>
2024-10-06 07:18:53 +0700

    feat: finish project & add metadata

--- app/meals/share/error.js
+++ app/meals/share/error.js
@@ -4,7 +4,7 @@ export default function Error() {
     return (
         <main className="error">
             <h1>An error occured!</h1>
-            <p>Filed to fetched the data, please try again</p>
+            <p>Filed to create meal</p>
         </main>
     )
 }
